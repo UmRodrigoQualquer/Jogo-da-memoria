af9f68b07e82ca653fa82437958f943c2029d64e
UmRodrigoQualquer <bboy.rodrigo1998@gmail.com>
2022-08-19 12:06:50 -0300

    :lipstick: Add max-width to body

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,12 +1,13 @@
+import "./style.css";
 import CardGame from "../../compomentes/CardGame";
 function BoardGame(amountCards) {
     const $htmlCardGamer = CardGame();
     const $htmlBoardGame = $htmlCardGamer.repeat(amountCards);
-
-    console.log($htmlBoardGame);
     
     
-    return $htmlBoardGame;
+    return `
+    <section class = "board-game">
+    ${$htmlBoardGame}</section>`;
     
 }
 
