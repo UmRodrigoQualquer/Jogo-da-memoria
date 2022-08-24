5f6c6001d9dbcaf8de8cbf343a173410304e61d2
UmRodrigoQualquer <bboy.rodrigo1998@gmail.com>
2022-08-24 13:15:03 -0300

    :sparkles: Add click event on CardFrontBack component

--- main.js
+++ main.js
@@ -13,6 +13,6 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
     "beforeend",`
         ${ScoreBoard()} 
-        ${BoardGame(2)}
+        ${BoardGame(6)}
        ` 
         ); 
--- src/Componentes/CardFrontBack/index.js
+++ src/Componentes/CardFrontBack/index.js
@@ -2,8 +2,24 @@ import CardGame from "../CardGame"
 import"./style.css"
 
 function cardFrontBack () {
+    window.cardFrontBack = {};//nameSpac
+    window.cardFrontBack.handleClick = (event) => {// toda essa parte do código é um evento de click
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest('.card-front-back');//preciso entender isso melhor. Voltar aqui depois!
+
+        /*if ($cardFrontBack.classList.contains('-active')) {
+            
+            $cardFrontBack.classList.remove('-active');
+
+        } else {
+            $cardFrontBack.classList.add("-active");
+        }*/
+
+        $cardFrontBack.classList.toggle('-active');
+    }
+    
     return /*html*/`
-    <article class = "card-front-back">
+    <article class = "card-front-back" onclick="cardFrontBack.handleClick(event)">
         <div class="card -front">
         ${CardGame()}
         </div>
